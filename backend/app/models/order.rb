4c71467146c53155c4f7b554f1ffd8a2f27c7fa5
dangtran.khoa <dangtran.khoa@nfq.com>
2025-04-30 10:53:30 +0700

    feat: init schema for order

---
+++ backend/app/models/order.rb
@@ -0,0 +1,8 @@
+class Order < ApplicationRecord
+  belongs_to :user, class_name: "User"
+  has_many :order_items, dependent: :destroy
+
+  validates :idempotency_key, presence: true, uniqueness: true
+  validates :total_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :status, presence: true, inclusion: { in: %w[pending completed failed] }
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_29_162733) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_30_035022) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -79,6 +79,27 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_29_162733) do
     t.index ["import_job_id"], name: "index_import_records_on_import_job_id"
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "asset_id", null: false
+    t.integer "amount", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["asset_id"], name: "index_order_items_on_asset_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "idempotency_key", null: false
+    t.integer "total_amount", null: false
+    t.string "status", default: "pending", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idempotency_key"], name: "index_orders_on_idempotency_key", unique: true
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -104,5 +125,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_29_162733) do
   add_foreign_key "assets", "users", column: "creator_id"
   add_foreign_key "import_jobs", "users", column: "creator_id"
   add_foreign_key "import_records", "import_jobs"
+  add_foreign_key "order_items", "assets"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
   add_foreign_key "users", "roles"
 end
