9501062e7ca4b6a4b59ac850f17f6b39e91e4027
Richard K Tran <richardktran.dev@gmail.com>
2025-04-30 12:08:17 +0700

    feat: total earnings for creators

---
+++ backend/app/controllers/api/v1/admin/statistics_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Admin::StatisticsController < Api::V1::Admin::AdminBaseController
+  def creators_earning
+    earnings = User
+            .left_outer_joins(:role, :orders)
+            .where(roles: { name: 'creator' })
+            .group('users.id')
+            .select('users.id AS creator_id, COALESCE(SUM(orders.total_amount), 0) AS total_earnings')
+            .map { |creator| { creator_id: creator.creator_id, total_earnings: creator.total_earnings.to_f } }
+
+    render json: { data: earnings }, status: :ok
+  end
+end
---
+++ backend/test/controllers/api/v1/admin/admin_base_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::V1::Admin::AdminBaseControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
