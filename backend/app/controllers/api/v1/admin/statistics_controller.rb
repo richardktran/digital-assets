37ae3d1fa2feb5831f0f1a82c23e1b37ffa5492e
Richard K Tran <richardktran.dev@gmail.com>
2025-04-30 17:26:29 +0700

    feat: build the total_earnings table

--- backend/app/controllers/api/v1/admin/statistics_controller.rb
+++ backend/app/controllers/api/v1/admin/statistics_controller.rb
@@ -4,12 +4,18 @@ class Api::V1::Admin::StatisticsController < Api::V1::Admin::AdminBaseController
             .left_outer_joins(:role, :orders)
             .where(roles: { name: 'creator' })
             .group('users.id')
-            .select('users.id AS creator_id, COALESCE(SUM(orders.total_amount), 0) AS total_earnings')
-            .map { |creator| { creator_id: creator.creator_id, total_earnings: creator.total_earnings.to_f } }
-
-    earnings.each do |creator|
-      creator[:total_earnings] /= 100
-    end
+            .select('users.id AS creator_id, users.first_name, users.last_name, users.email, COALESCE(SUM(orders.total_amount), 0) AS total_earnings')
+            .map do |creator| 
+              { 
+                creator: {
+                  id: creator.creator_id,
+                  first_name: creator.first_name,
+                  last_name: creator.last_name, 
+                  email: creator.email
+                },
+                total_earnings: creator.total_earnings.to_f / 100
+              }
+            end
 
     render json: { data: earnings }, status: :ok
   end
